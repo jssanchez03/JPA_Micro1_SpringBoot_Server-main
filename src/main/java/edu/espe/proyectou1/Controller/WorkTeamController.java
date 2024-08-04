603b0f5b54678310cbb24f4509c10b044b020df1
Jair Sánchez <jssanchez9@espe.edu.ec>
2024-08-04 10:50:54 -0500

    Backend de microservicio 1

--- src/main/java/edu/espe/proyectou1/Controller/WorkTeamController.java
+++ src/main/java/edu/espe/proyectou1/Controller/WorkTeamController.java
@@ -5,7 +5,6 @@ import edu.espe.proyectou1.Service.WorkTeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
